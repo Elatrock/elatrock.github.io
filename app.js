3e1c2b05b4c18e246ebf1374f7f988e5e5fb42c6
Elatrock <83244290+Elatrock@users.noreply.github.com>
2023-07-12 19:36:56 -0500

    Añadidas funciones para editar y eliminar registros.

--- app.js
+++ app.js
@@ -61,4 +61,127 @@ function leer() {
     }
 }
 
+// Función Editar
+
+function editar(nombre) {
+    let alumnos = JSON.parse(localStorage.getItem('alumnos'));
+
+    for (let i=0; i < alumnos.length; i++) {
+        if (alumnos[i].nombre === nombre) {
+            document.getElementById('body').innerHTML =
+            `
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-header">
+                            <h2>Editar libro</h2>
+                        </div>
+                        <div class="card-body">
+                            <form>
+                                <div class="form-group">
+                                    <input type="text" id="newnombre" class="form-control my-3" placeholder="${alumnos[i].nombre}">
+                                </div>
+                                <div>
+                                    <input type="email" id="newcorreo" class="form-control my-3" placeholder="${alumnos[i].correo}">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" id="newgrado" class="form-control my-3" placeholder="${alumnos[i].grado}">
+                                </div>
+                                <div class="form-group">
+                                    <input type="number" id="newcalificacion" class="form-control my-3" placeholder="${alumnos[i].calificacion}">
+                                </div>
+                            </form>
+                            <button type="submit" class="btn btn-success" onclick="actualizar('${i}')">Actualizar</button>
+                            <button type="submit" class="btn btn-primary" onclick="vistaPrincipal()">Cancelar</button>
+
+                        </div>
+                    </div>
+            `
+        }
+    }
+}
+
+// Función Actualizar
+function actualizar(i) {
+    let alumnos = JSON.parse(localStorage.getItem('alumnos'));
+
+    alumnos[i].nombre = document.getElementById('newnombre').value;
+    alumnos[i].correo = document.getElementById('newcorreo').value;
+    alumnos[i].grado = document.getElementById('newgrado').value;
+    alumnos[i].calificacion = document.getElementById('newcalificacion').value;
+
+    localStorage.setItem('alumnos', JSON.stringify(alumnos));
+    console.log('Registro actualizado');
+    vistaPrincipal();
+}
+
+// Función Eliminar
+function eliminar(nombre) {
+    let alumnos = JSON.parse(localStorage.getItem('alumnos'));
+
+    for(let i=0; i < alumnos.length; i++) {
+        if (alumnos[i].nombre === nombre) {
+            alumnos.splice(i, 1);
+            console.log('Registro eliminado');
+        }
+    }
+
+    localStorage.setItem('alumnos', JSON.stringify(alumnos));
+    leer();
+}
+
+// Función Vista Principal
+function vistaPrincipal() {
+    document.getElementById('body').innerHTML =
+    `
+    <div class="row">
+        <div class="col-md-5">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Registrar alumno</h3>
+                </div>
+                <div class="card-body">
+                    <form id="formulario">
+                        <div class="form-group">
+                            <input type="text" id="nombre" class="form-control my-3" placeholder="Nombre">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" id="email" class="form-control my-3" placeholder="Correo electrónico">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" id="grado" class="form-control my-3" placeholder="Grado"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <input type="number" id="calificacion" class="form-control my-3" placeholder="Calificación">
+                        </div>
+                        <button type="submit" class="btn btn-success">Agregar</button>
+                        <button type="reset" class="btn btn-danger">Limpiar</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <table class="table caption-top bg-light">
+                <thead>
+                    <tr>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Apellido</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Grado</th>
+                    </tr>
+                </thead>
+                <tbody id="tbody">
+                    <tr>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    `
+
+    console.log('Registro no actualizado')
+    leer();
+}
+
 leer();
